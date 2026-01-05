beeb4276147ed6828198d70a50809faed6aa50e3
allscorpion <deadlyryz@hotmail.co.uk>
2026-01-05 21:31:14 +0000

    setting up tf and idf

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import math
 from parse_tokens import parse_tokens, contains_token
 from movie_cache import InvertedIndex
 
@@ -47,6 +48,66 @@ def handle_build():
     cacher.save()
 
 
+def handle_tf(document_id: int, term: str):
+    try:
+        cacher.load()
+    except:
+        print("please run build command before running this command")
+        return
+    tf = cacher.get_tf(document_id, term)
+    print(f"{document_id} contains the term {term}, {tf} time(s)")
+
+
+def get_idf(term: str):
+    tokens = parse_tokens(term)
+
+    if len(tokens) != 1:
+        print("only provide a single token")
+
+    token = tokens[0]
+
+    total_doc_count = len(cacher.docmap)
+    term_match_doc_count = len(cacher.get_documents(token))
+    idf = math.log((total_doc_count + 1) / (term_match_doc_count + 1))
+
+    return idf
+
+
+def handle_idf(term: str):
+    try:
+        cacher.load()
+    except:
+        print("please run build command before running this command")
+        return
+
+    idf = get_idf(term)
+    print(f"Inverse document frequency of '{term}': {idf:.2f}")
+
+
+def handle_tfidf(document_id: int, term: str):
+    try:
+        cacher.load()
+    except:
+        print("please run build command before running this command")
+        return
+
+    tf = cacher.get_tf(document_id, term)
+    idf = get_idf(term)
+    tf_idf = tf * idf
+
+    print(f"TF-IDF score of '{term}' in document '{document_id}': {tf_idf:.2f}")
+
+
+def handle_bm25_idf(term: str):
+    try:
+        cacher.load()
+    except:
+        print("please run build command before running this command")
+        return
+    bm25_idf = cacher.get_bm25_idf(term)
+    print(f"BM25 IDF score of '{term}': {bm25_idf:.2f}")
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -55,6 +116,27 @@ def main() -> None:
     search_parser.add_argument("query", type=str, help="Search query")
 
     subparsers.add_parser("build", help="Build movies into cache")
+    term_frequency_parser = subparsers.add_parser(
+        "tf", help="Check how frequent a term is"
+    )
+    term_frequency_parser.add_argument(
+        "document_id", type=int, help="The document ID to check"
+    )
+    term_frequency_parser.add_argument("term", type=str, help="The term to lookup")
+
+    idf_parser = subparsers.add_parser("idf", help="Check how common a term is")
+    idf_parser.add_argument("term", type=str, help="The term to lookup")
+
+    tf_idf_parser = subparsers.add_parser("tfidf", help="Check how common a term is")
+    tf_idf_parser.add_argument("document_id", type=int, help="The document ID to check")
+    tf_idf_parser.add_argument("term", type=str, help="The term to lookup")
+
+    bm25_idf_parser = subparsers.add_parser(
+        "bm25idf", help="Get BM25 IDF score for a given term"
+    )
+    bm25_idf_parser.add_argument(
+        "term", type=str, help="Term to get BM25 IDF score for"
+    )
 
     args = parser.parse_args()
 
@@ -63,6 +145,14 @@ def main() -> None:
             handle_search(args.query)
         case "build":
             handle_build()
+        case "tf":
+            handle_tf(args.document_id, args.term)
+        case "idf":
+            handle_idf(args.term)
+        case "tfidf":
+            handle_tfidf(args.document_id, args.term)
+        case "bm25idf":
+            handle_bm25_idf(args.term)
         case _:
             parser.print_help()
 
--- cli/movie_cache.py
+++ cli/movie_cache.py
@@ -1,3 +1,5 @@
+from collections import Counter
+import math
 from parse_tokens import parse_tokens
 
 import pickle
@@ -8,15 +10,18 @@ class InvertedIndex:
     def __init__(self) -> None:
         self.index: dict[str, set[int]] = {}
         self.docmap = {}
+        self.term_frequencies: dict[int, Counter] = {}
 
     def __add_document(self, doc_id: int, text: str):
         tokens = parse_tokens(text)
+        self.term_frequencies[doc_id] = Counter()
 
         for token in tokens:
             if token not in self.index:
                 self.index[token] = set()
 
             self.index[token].add(doc_id)
+        self.term_frequencies[doc_id].update(tokens)
 
     def get_documents(self, term: str) -> list[int]:
         doc_ids = self.index.get(term.lower(), set())
@@ -27,6 +32,32 @@ class InvertedIndex:
             self.__add_document(movie["id"], f"{movie['title']} {movie['description']}")
             self.docmap[movie["id"]] = movie
 
+    def parse_term_into_single_token(self, term):
+        parsed_terms = parse_tokens(term)
+
+        if len(parsed_terms) != 1:
+            raise ValueError("term is invalid, please supply one valid term")
+
+        parsed_term = parsed_terms[0]
+
+        return parsed_term
+
+    def get_tf(self, doc_id, term):
+        token = self.parse_term_into_single_token(term)
+        return self.term_frequencies[doc_id].get(token, 0)
+
+    def get_bm25_idf(self, term: str) -> float:
+        token = self.parse_term_into_single_token(term)
+        total_doc_count = len(self.docmap)
+        term_match_doc_count = len(self.get_documents(token))
+        bm25_idf = math.log(
+            (total_doc_count - term_match_doc_count + 0.5)
+            / (term_match_doc_count + 0.5)
+            + 1
+        )
+
+        return bm25_idf
+
     def save(self):
         Path("cache").mkdir(parents=True, exist_ok=True)
         with open("cache/index.pkl", "wb") as f:
@@ -35,8 +66,13 @@ class InvertedIndex:
         with open("cache/docmap.pkl", "wb") as f:
             pickle.dump(self.docmap, f)
 
+        with open("cache/term_frequencies.pkl", "wb") as f:
+            pickle.dump(self.term_frequencies, f)
+
     def load(self):
         with open("cache/index.pkl", "rb") as f:
             self.index = pickle.load(f)
         with open("cache/docmap.pkl", "rb") as f:
             self.docmap = pickle.load(f)
+        with open("cache/term_frequencies.pkl", "rb") as f:
+            self.term_frequencies = pickle.load(f)
